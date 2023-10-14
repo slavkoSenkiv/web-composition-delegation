607946cefc56d2faf225daee2f11cee14386db9b
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-10-14 08:57:34 +0200

    finish ApiSync class and its connection with Model class

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,5 +1,5 @@
+import axios, { AxiosPromise, AxiosResponse } from 'axios';
 
-import { Attributes } from "./Attributes";
 
 interface ModelAttributes<T> {
   get<K extends keyof T>(key: K): T[K];
@@ -7,14 +7,55 @@ interface ModelAttributes<T> {
   getAll(): T;
 }
 
+interface Events {
+  on(eventName: string, callback: () => {}): void;
+  trigger(eventName: string): void;
+}
+
 interface HasId {
   id?: number;
 }
 
+interface Sync<T> {
+  rootUrl: string; //discremencies
+  fetch (id: number): AxiosPromise;
+  save (data: T): AxiosPromise;
+}
+
 export class Model<T extends HasId> {
     constructor (
-      private attributes: ModelAttributes<T>
+      private attributes: ModelAttributes<T>,
+      private sync: Sync<T>,
+      private events: Events
     ) { }
 
   get = this.attributes.get;
+  on = this.events.on;
+  trigger = this.events.trigger;
+
+  set (update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch (): void {
+    let id = this.get('id'); // discrepencies
+    if (typeof(id) != 'number') {
+      throw new Error ('can not fetch without an ID');
+    }
+    this.sync.fetch(id)
+      .then ((response: AxiosResponse) => {
+        this.set(response.data);
+      });
+  }
+
+  save (): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error')
+      });
+  }
 }
