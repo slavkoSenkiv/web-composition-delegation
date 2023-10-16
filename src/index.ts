923871ed4971993f11088caef6cdb10d1916a980
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-10-16 08:33:58 +0200

    add user form class and render it via index ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-/* import { User } from "./models/User";
+import { UserForm } from "./views/UserForm";
 
-const user1 = User.buildUser({ id: 1});
-user1.on('change', () => console.log(user1));
-user1.fetch();
+const userForm = new UserForm(
+  document.getElementById('root')
+);
 
-const user2 = User.buildUser({name: 'petro', age: 32});
-user2.save();
- */
-
-/* import axios, { AxiosResponse } from "axios";
-
-axios.get('http://localhost:3000/users')
-  .then((response: AxiosResponse) => {
-    console.log(response.data);
-    
-  }) */
-import { User } from "./models/User"
-const collection = User.buildUserCollection();
-collection.on('change', () => {
-  console.log(collection);
-});
-collection.fetch();
+userForm.render();
