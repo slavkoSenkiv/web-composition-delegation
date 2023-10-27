983fcbda39fe09d58080ece7ec63fce0f60ff152
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-10-27 06:46:34 +0200

    try to rewrite the code alone

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,54 +1,42 @@
-import { User, UserProps } from "../models/User";
-import { View } from "./View";
-
-export class UserForm extends View<User, UserProps> {
 
   /* 
   lists what elements should have what event event listeners 
   and actions for those event listeners 
   later it is used by parent class bindEvents method  */
-  eventsMap(): { [key: string]: () => void; } {
-    return {
-      'click:.set-name': this.onSetNameClick,
-      'click:.set-age': this.onSetAgeClick,
-      'click:.save-model': this.onSaveClick
-    }
-  }
+
 
   /* 
   used as callback for event click event listener */
-  onSetNameClick = (): void => {
-    let input = document.querySelector('input');
-    if (input) {
-      let name = input.value;
-      this.model.set({name});
-    }
-  }
 
   /* 
   used as callback for event click event listener */
-  onSetAgeClick = (): void => {
-    this.model.setRandomAge();
-  }
-  
+
   /* 
   used as callback for event click event listener */
-  onSaveClick = (): void => {
-    this.model.save();
-  }
 
   /* 
   is html string used by parent class render method later */
-  template(): string {
-    return `
-    <div>
-      <input 
-        placeholder=${this.model.get('name')} 
-        name="nameInput" />
-      <button class="set-name">Change Name</button>
-      <button class="set-age">Set Random Age</button>
-      <button class="save-model">Save User</button>
-    </div>
-    `;
-  }
+
+import { User } from "../models/User";
+import { View } from "./View";
+
+  export class UserForm extends View<User> {
+
+    template(): string {
+      return `
+        <div>
+          <h1> User Form </h1>
+          <p>
+            user name: ${this.model.get('name')}
+            </br>
+            user age: ${this.model.get('age')}
+          </p>
+          <input />
+          <button> Set Name </button>
+          <button> Set Age </button>
+          <button> Save User </button>
+        </div>
+      `
+    }
+
 }
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,81 +1,38 @@
-import { HasId, Model } from "../models/Model";
-
-export abstract class View<T extends Model<K>, K extends HasId> {
-
-  regions: { [key: string]: Element } = {};
-
-  constructor(
-    public parent: Element,
-    public model: T
-  ) {
-    this.bindModel();
-  }
 
   /* 
   child class is required to have method with similar name, 
   and it over this method with child's method */
-  abstract template(): string;
+
+
   
   /* 
   if child class has similar name method, 
   it over this method with child's method */
-  onRender(): void {
-  }
   
   /* 
   if child class has similar name method, 
   it over this method with child's method */
-  eventsMap(): {[key: string]: ()=> void} {
-     return {};
-  }
 
   /* 
   if child class has similar name method, 
   it over this method with child's method */
-  regionsMap(): { [key: string]: string } {
-    return {};
-  }
 
   /* 
   makes page rerender every time when 
   'change' event is triggered in the data model
   by lising render method as one of 'change' event callbacks */
-  bindModel(): void {  
-    this.model.on('change', () => {
-      this.render();
-    });
-  }
 
   /* 
   grabs event listeners and their callbacks 
   from events map specified in the child class
   and attaches these events to corresponding 
   html elements  as event listeners */
-  bindEvents(fragment: DocumentFragment): void {
-    let eventsMap = this.eventsMap();
-    for (let eventKey in eventsMap) {
-      let [eventName, selector] = eventKey.split(':');
-      fragment.querySelectorAll(selector).forEach((element)=>{
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
 
   /* 
   grabs selectors from child class regions map method
   and builds collection with the same keys 
   with properties as html elements, 
   not css class selectors*/
-  mapRegions(fragment: DocumentFragment): void {
-    let regionsMap = this.regionsMap();
-    for (let key in regionsMap) {
-      let selector = regionsMap[key];
-      let element = fragment.querySelector(selector);
-      if (element) {
-        this.regions[key] = element;
-      }
-    }
-  }
   
   /* 
   rerenders page content
@@ -88,14 +45,23 @@ export abstract class View<T extends Model<K>, K extends HasId> {
   5 - identifies all the regions should be nested
   6 - renders each of them and drills down untill there are no nested elements
   7 - nests this <template> with all the nested stuff to parent (root div)*/
+
+import { User } from "../models/User";
+
+export abstract class View<T extends User> {
+
+  constructor (
+    private parent: Element,
+    public model: T
+  ) {};
+
+  abstract template(): string;
+
   render(): void {
     this.parent.innerHTML = '';
-    let templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-    this.bindEvents(templateElement.content);
-    this.mapRegions(templateElement.content);
-    this.onRender();
-    this.parent.append(templateElement.content);
+    let tempateElement = document.createElement('template');
+    tempateElement.innerHTML = this.template();
+    this.parent.append(tempateElement.content);
   }
 
 }
